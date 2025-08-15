75c120b81b506a085174eef3b087ba7f42fea5fd
Sanshrit <sanshritbakshi@gmail.com>
2025-08-15 14:52:05 +0530

    quizzes progress server app

--- Kambaz/Quizzes/dao.js
+++ Kambaz/Quizzes/dao.js
@@ -1,8 +1,9 @@
 import model from "./model.js";
+import { v4 as uuidv4 } from "uuid";
 
 export const findAllCourseQuizzes = (cid) => model.find({ course: cid });
 export const createQuiz = (quiz) => {
-    return model.create(quiz);
+    return model.create({ ...quiz, _id: uuidv4() });
 };
 export const deleteQuiz = (quizId) => model.deleteOne({ _id: quizId });
 export const updateQuiz = (quizId, quiz) => model.updateOne({ _id: quizId }, { $set: quiz });
--- Kambaz/Users/schema.js
+++ Kambaz/Users/schema.js
@@ -16,6 +16,7 @@ const userSchema = new mongoose.Schema({
     section: String,
     lastActivity: Date,
     totalActivity: String,
+    quizAttempts: Array,
 },
     { collection: "users" }
 );
