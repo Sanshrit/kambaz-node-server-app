16f936ee311fbccc91413a2bcddbb2c9a50a9b40
Sanshrit <sanshritbakshi@gmail.com>
2025-08-18 12:50:27 +0530

    fixing orphaned enrollments

--- Kambaz/Quizzes/dao.js
+++ Kambaz/Quizzes/dao.js
@@ -1,10 +1,17 @@
 import model from "./model.js";
 import { v4 as uuidv4 } from "uuid";
+import userModel from "../Users/model.js";
 
 export const findAllCourseQuizzes = (cid) => model.find({ course: cid });
 export const createQuiz = (quiz) => {
     return model.create({ ...quiz, _id: uuidv4() });
 };
-export const deleteQuiz = (quizId) => model.deleteOne({ _id: quizId });
+export const deleteQuiz = async (quizId) => {
+    await userModel.updateMany(
+        { "quizAttempts.quiz": quizId },
+        { $pull: { quizAttempts: { quiz: quizId } } }
+    )
+    await model.deleteOne({ _id: quizId })
+};
 export const updateQuiz = (quizId, quiz) => model.updateOne({ _id: quizId }, { $set: quiz });
 export const findOneQuiz = (cid, qid) => model.find({ course: cid, _id: qid });
